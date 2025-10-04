0ccea8bc584e94314a9f738cd083cd1b08a1b654
Mahendra <mahendranayak364@gmail.com>
2025-10-04 23:47:01 +0530

    server upd

--- middleware.js
+++ middleware.js
@@ -54,7 +54,9 @@ module.exports.validateReview = (req, res, next) => {
 };
 
 module.exports.isReviewAuthor = async (req,res,next) => {
-    let { id , reviewId } = req.params;
+    let { reviewId } = req.params;
+    // Extract id from the URL path since we're not using mergeParams
+    const id = req.originalUrl.split('/')[2]; // /listings/:id/reviews/:reviewId
         let review = await Review.findById(reviewId);
         if (!review.author.equals(req.user._id)) {
             req.flash("error", "You are not the author of this review");
--- routes/review.js
+++ routes/review.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router({mergeParams:true});
+const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js");
 
